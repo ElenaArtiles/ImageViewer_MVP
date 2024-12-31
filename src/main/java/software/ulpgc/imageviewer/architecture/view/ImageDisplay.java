78b07e18b2d50d105343631f20d2ca57ffd182de
ElenaArtiles <morartielen@gmail.com>
2024-12-31 01:05:31 +0000

    Control package deleted. ImageDisplay interface updated

--- src/main/java/software/ulpgc/imageviewer/architecture/view/ImageDisplay.java
+++ src/main/java/software/ulpgc/imageviewer/architecture/view/ImageDisplay.java
@@ -5,7 +5,20 @@ import software.ulpgc.imageviewer.architecture.model.Image;
 import java.awt.*;
 
 public interface ImageDisplay {
-    void show(Image image);
-    Image image();
-    void paint(Graphics g);
+    int width();
+    void clear();
+    void paint(String image, int offset);
+
+    void on(Dragged dragged);
+    void on(Released released);
+
+    interface Dragged {
+        Dragged Null = offset -> {};
+        void to(int offset);
+    }
+
+    interface Released {
+        Released Null = offset -> {};
+        void at(int offset);
+    }
 }
